b4344e56a1094d9c70dfe0fe5ffec095769109d9
Bart Heylen <bartheylen94@gmail.com>
2017-02-18 15:28:54 +0100

    first try of initialization class BONDS

--- code/PART 1.py
+++ code/PART 1.py
@@ -1,40 +1,24 @@
 #initialize class BOND
 class Bond(object):
     #specify properties
-    def __init__(self, term):
+    def __init__(self, term, amount_inv, i_rate):
         self.term = term
-
-    def __init__(self, amount_inv):
         self.amount_inv = amount_inv
-
-    def __init__(self, min_price):
-        self.min_price = min_price
-
-    def __init__(self,min_term):
-        self.min_term = min_term
-
-    def __init(self,i_rate):
         self.i_rate = i_rate
 
-
-     #specify method(s)
+        #specify method(s)
     def inv_return(self, term, amount_inv, i_rate):
         return (amount_inv*(1 + i_rate)**(term))
 
-    def compound_return(self, term, amount_inv, i_rate):
-        total = inv_return(self, term, amount_inv, i_rate)
-        return ((total/amount_inv)**(1/term)-1)
+    def compound_return(self, term, i_rate):
+        return ((1+i_rate)**term)-1)
 
 class STBond(Bond):
-    def __init__(self, min_term):
+    def __init__(self, min_term, min_price):
         self.min_term = 2
-
-    def __init__(self, min_price):
         self.min_price = 1000
 
 class LTBond(Bond):
-    def __init__(self, min_term):
+    def __init__(self, min_term, min_price):
         self.min_term = 5
-
-    def __init__(self, min_price):
         self.min_price = 3000
