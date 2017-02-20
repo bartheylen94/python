e4e874565102465618cdf005cb92f949ed13eec1
Bart Heylen <bartheylen94@gmail.com>
2017-02-20 22:48:50 +0100

    defining classes 101

--- code/Class Bond.py
+++ code/Class Bond.py
@@ -1,15 +1,12 @@
 #initialize class BOND
 class Bond(object):
     #specify properties
-    def __init__(self, term, amount_inv, i_rate):
-        self.term = term
-        self.amount_inv = amount_inv
+    def __init__(self, min_term, i_rate, min_amount):
+        self.min_term = min_term
+        self.min_amount = min_amount
         self.i_rate = i_rate
 
         #specify method(s)
-    def inv_return(self, term, amount_inv, i_rate):
-        return (amount_inv*(1 + i_rate)**(term))
-
     def compound_return(self, term, i_rate):
         return ((1+i_rate)**term)-1)
 
---
+++ code/Class Investor.py
@@ -0,0 +1,13 @@
+#initialize class Investor, 3 subclasses
+class Investor(object):
+    def __init__(self, Budget, StartDate)
+        self.Budget = Budget
+        self.StartDate = StartDate
+
+    #methods
+
+
+#subclasses
+class Defensive(Investor):
+    #return method?
+
