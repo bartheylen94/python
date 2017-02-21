37e87dba970c5c9ecfb71454f72691a69041d950
Bart Heylen <bartheylen94@gmail.com>
2017-02-21 17:02:59 +0100

    adaptions 17.02 21/02/2017

--- code/Class Bond.py
+++ code/Class Bond.py
@@ -1,14 +1,23 @@
 #initialize class BOND
 class Bond(object):
     #specify properties
-    def __init__(self, min_term, i_rate, min_amount):
+    def __init__(self, min_term, i_rate, min_amount, inv_time, quantity):
         self.min_term = min_term
         self.min_amount = min_amount
         self.i_rate = i_rate
+        self.inv_term = inv_time    #expressed in days; 360 per year
+        self.quantity = quantity
 
-        #specify method(s)
-    def compound_return(self, term, i_rate):
-        return ((1+i_rate)**term)-1)
+    #specify method(s)
+        #compound return, yearly compounding
+    def compound_return(self):
+        return ((1+self.i_rate)**(self.inv_term/360)-1)
+
+    def yearly_coupon(self):
+        comp_r = compound_return(self.i_rate, self.inv_term)
+        ycr = comp_r**(1/self.inv_term)
+        return ycr*self.quantity
+
 
 #define subclasses for short and long term bonds
 class STBond(Bond):
