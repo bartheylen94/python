c6195c41694117fcd8b0202d734238df9bb5ed38
Bart Heylen <bartheylen94@gmail.com>
2017-02-21 22:44:30 +0100

    21.02.2017 Bart Heylen next step in the initialization of class Bond

--- code/Class Bond.py
+++ code/Class Bond.py
@@ -14,18 +14,20 @@ class Bond(object):
         return ((1+self.i_rate)**(self.inv_term/360)-1)
 
     def yearly_coupon(self):
-        comp_r = compound_return(self.i_rate, self.inv_term)
+        comp_r = self.compound_return
         ycr = comp_r**(1/self.inv_term)
         return ycr*self.quantity
 
 
 #define subclasses for short and long term bonds
 class STBond(Bond):
-    def __init__(self, min_term, min_amount):
+    def __init__(self, min_term, i_rate, min_amount, inv_time, quantity):
+        Bond.__init__(self, min_term, i_rate, min_amount, inv_time, quantity)
         self.min_term = 2
         self.min_amount = 1000
 
 class LTBond(Bond):
-    def __init__(self, min_term, min_amount):
+    def __init__(self, min_term, i_rate, min_amount, inv_time, quantity):
+        Bond.__init__(self, min_term, i_rate, min_amount, inv_time, quantity)
         self.min_term = 5
         self.min_amount = 3000
