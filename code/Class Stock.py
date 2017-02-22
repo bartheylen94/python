c25e3e4bdb61d6660982e74e69e591c3849e825b
Luca Fiorentino <fiorelux@gmail.com>
2017-02-22 10:02:17 +0100

    I modify the class stock

--- code/Class Stock.py
+++ code/Class Stock.py
@@ -1,14 +1,24 @@
+#---------------------------
+# CLASS STOCK
+# Authors: luca.fiorentino@iae-toulouse.fr , bart.heylen@iae-toulouse.fr
+# Note:the class use yahoo finance API to have the quotations of the stocks
+#---------------------------
+#import library
+    import pandas_datareader.data as web
+    import datetime
 # initialize class Stock
 class Stock(object):
     #properties
-    def __init__(self, StockNumber, StockName, StartDate, EndDate):
+    def __init__(self, StockNumber, StockTkr, StartDate, EndDate):
         self.StockNumber = StockNumber
-        self.StockName = StockName
+        self.StockTkr = StockTkr
         self.StartDate = StartDate
         self.EndDate = EndDate
 
     #methods
         #def stockreturn(self):
-
-
+    def __init__(self, startDate, endDate):
+        # import stock data
+        quotes = web.DataReader(self.StockTkr, 'yahoo', startDate, endDate)
+        return quotes
 
