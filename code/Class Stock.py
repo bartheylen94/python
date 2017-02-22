2f1c365b53f4bc3ce0bf8b389faaed9307631ce8
Luca Fiorentino <fiorelux@gmail.com>
2017-02-22 12:04:19 +0100

    I modify the class stock

--- code/Class Stock.py
+++ code/Class Stock.py
@@ -4,11 +4,11 @@
 # Note:the class use yahoo finance API to have the quotations of the stocks
 #---------------------------
 #import library
+
     from math import log
-    import pandas
     import pandas_datareader.data as web
-    import datetime
-# initialize class Stock
+
+#initialize class Stock
 class Stock(object):
     #properties
     def __init__(self, StockNumber, StockTkr, StartDate, EndDate):
@@ -24,12 +24,14 @@ class Stock(object):
         quotes = web.DataReader(self.StockTkr, 'yahoo', startDate, endDate)
         quotes['dates'] = quotes.index.map(lambda x: str(x)[:10])
         return quotes
-    # compute the continously-compounded return of a stock in a period
+
+        # compute the continously-compounded return of a stock in a period
     def stkCCReturn(self,startReturn, endReturn):
         quotesReturn=self.getQuotes(startReturn, endReturn)
-        quotesReturn['CCStkReturn'] = quotesReturn['CCStkReturn'].map(lambda x: log(quotesReturn[i, 'Close']/quotesReturn[i-1, 'Close']) for i in quotesReturn.Count )
+        quotesReturn['CCStkReturn'] = quotesReturn['CCStkReturn'].map(lambda x: log(quotesReturn[i, 'Close']/quotesReturn[i-1, 'Close']) for i in len(quotesReturn) )
         return quotesReturn.sum('CCStkReturn')
-    # comute the daily volatility
+
+        # compute the daily volatility
     def stkVolatility(self, startVol, endVol):
         quotesVol=self.getQuotes(startVol, endVol)
-        quotesVol['CCStkReturn']=quotesVol['CCStkReturn'].map(lambda x: log(quotesVol[i, 'Close']/quotesVol[i-1, 'Close']) for i in quotesVol.Count )
+        quotesVol['DVolatility']=quotesVol['DVolatility'].map(lambda x: ((quotesVol[i,'CCStkReturn']-quotesVol.mean('CCStkReturn'))**2) for i in len(quotesVol))
