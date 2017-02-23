eca9846e9c82458e26b516337d2fde0bd3ce0102
Luca Fiorentino <fiorelux@gmail.com>
2017-02-23 11:43:24 +0100

    I changed the method of class stock

--- code/Simulations.py
+++ code/Simulations.py
@@ -1,8 +1,12 @@
-from .Stock import Stock
-import datetime
+from Stock import Stock
+from datetime import datetime,
 
 start = datetime.datetime(2016, 1, 1)
 end = datetime.datetime(2017, 2, 27)
+if start.isocalendar() == 6:
+    start=start - 1
+if start.isocalendar() == 7:
+    start=start +1
 
 stk=Stock(1,'GOOGL',start,end)
 stk.getQuotes(start,end)
